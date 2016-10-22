c40736742135cb7280caf0b4d6be6f5a5b2dea91
lunaroyster <oneinaquadrillion@live.com>
2016-10-22 07:56:05 +0000

    Done things: models npm

--- models/activitytemplate.js
+++ models/activitytemplate.js
@@ -1,2 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var activityTemplateSchema = new Schema({
+   owner: {
+       ownerType: String,
+       ownerID: String
+   },
+   createdDate: Date,
+   questions: 
+});
+
+module.exports = mongoose.model('activityTemplate', activityTemplateSchema);
