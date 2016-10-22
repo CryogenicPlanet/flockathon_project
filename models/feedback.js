c12c3adac982303546424ec684431f1fb9521688
lunaroyster <oneinaquadrillion@live.com>
2016-10-22 12:52:13 +0000

    Cleaned up feedback code

--- models/feedback.js
+++ models/feedback.js
@@ -5,28 +5,28 @@ var feedbackSchema = new Schema({
     title : String,
     content: String,
     createdDate : Date
-    });
+});
     
-    feedbackSchema.statics.addFeedback = function(tn,cn){
-        var f = new Feedback({
-            title : tn,
-            content : cn,
-            createdDate: Date.now()
-        });
-         f.save(function (err) {
+feedbackSchema.statics.addFeedback = function(tn,cn) {
+    var f = new Feedback({
+        title : tn,
+        content : cn,
+        createdDate: Date.now()
+    });
+    f.save(function (err) {
         if (err) {
             return err;
         }
         else {
-            console.log("Post saved");
+            console.log("Feedback Saved");
         }
     });
-    };
-    feedbackSchema.statics.getLatest = function(){
-    return feedback.find().limit(25).sort(createdDate);
-        
-    }
-    
-    var Feedback  = mongoose.model('Feedback', feedbackSchema);
+};
+
+feedbackSchema.statics.getLatest = function(){
+    return Feedback.find().limit(25).sort('createdDate');
+}
+
+var Feedback  = mongoose.model('Feedback', feedbackSchema);
 
 module.exports = Feedback;
