fe298a0cc75d7663f31fbd0b6c53769612f82f23
lunaroyster <oneinaquadrillion@live.com>
2016-10-22 06:01:47 +0000

    Added libraries. Organised code. Please keep code clean.

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
+// Environment Variables
+var port = process.env.PORT;
+
+// Server
 var express = require('express');
 var app = express();
+var server = app.listen(port);
+
+// Libraries
+var bodyParser = require('body-parser');
+var requestify = require('requestify');
+var http = require('http');
