6b9b973455c2aade0bf803cc4b9b9d5f1fea753f
lunaroyster <oneinaquadrillion@live.com>
2016-10-22 13:47:54 +0000

    Activity template add method

--- models/activitytemplate.js
+++ models/activitytemplate.js
@@ -18,13 +18,27 @@ var activityTemplateSchema = new Schema({
 
 activityTemplateSchema.statics.getList = function(n, accessType, id){
    if (accessType == "team") {
-      return ActivityTemplate.find().limit(n);  //TODO check admin or group list
+      return ActivityTemplate.find({owner.ownerID: id}).limit(n);  //TODO check admin or group list
    }
    else if (accessType == "admin") {
       return ActivityTemplate.find().limit(n);  //TODO check admin or group list
    }
 }
 
+activityTemplateSchema.statics.add = function(oType, oID){
+  var at = new ActivityTemplate({
+     owner.ownerType: oType,
+     owner.ownerID: oID
+  });
+  at.save(function (err) {
+        if (err) {
+            return err;
+        }
+        else {
+            console.log("New activity template created.");
+        }
+    });
+};
 
 
 var ActivityTemplate = mongoose.model('activityTemplate', activityTemplateSchema);
