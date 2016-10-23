acb60c1f15b77c84fbabcf1f9b35c1c3edaa015c
Rahul Tarak <rahultarak12345@gmail.com>
2016-10-23 00:40:24 +0000

    review

--- models/activitytemplate.js
+++ models/activitytemplate.js
@@ -2,57 +2,54 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var questionSchema = new Schema({
-   questionType: String,
-   content: String
+   //questionType: String,
+   content: String,
+   quesID : Number
 });
 
 var activityTemplateSchema = new Schema({
-   owner: {
-       ownerType: String,
-       ownerID: String
-   },
    createdDate: Date,
    questions: [questionSchema]
+   
 });
 
 
-activityTemplateSchema.statics.getList = function(n, accessType, id){
-   // if (accessType == "team") {
-   //    return ActivityTemplate.find({'owner.ownerID': id}).limit(n);  //TODO check admin or group list
-   // }
-   // else if (accessType == "admin") {
-   //    return ActivityTemplate.find().limit(n);  //TODO check admin or group list
-   // }
-   // return ActivityTemplate.find({'owner.ownerID': id}).limit(n);
+// activityTemplateSchema.statics.getList = function(n, accessType, id){
+//   // if (accessType == "team") {
+//   //    return ActivityTemplate.find({'owner.ownerID': id}).limit(n);  //TODO check admin or group list
+//   // }
+//   // else if (accessType == "admin") {
+//   //    return ActivityTemplate.find().limit(n);  //TODO check admin or group list
+//   // }
+//   // return ActivityTemplate.find({'owner.ownerID': id}).limit(n);
    
-   var query = ActivityTemplate.find({'owner.ownerID': id}).limit(n);
+//   var query = ActivityTemplate.find({'owner.ownerID': id}).limit(n);
     
-    query.exec(function(err, ActivityTemplateList){
-        if (!err) {
-            return(ActivityTemplateList);
-        }
-    });
-};
+//     query.exec(function(err, ActivityTemplateList){
+//         if (!err) {
+//             return(ActivityTemplateList);
+//         }
+//     });
+// };
 
-activityTemplateSchema.statics.add = function(oType, oID){
-  var at = new ActivityTemplate({
-     'owner.ownerType': oType,
-     'owner.ownerID': oID
-  });
-  at.save(function (err) {
-        if (err) {
-            return err;
-        }
-        else {
-            console.log("New activity template created.");
-        }
-    });
-   return(at);
-};
-
-activityTemplateSchema.methods.addQuestion = function(qType, cont){
+// activityTemplateSchema.statics.add = function(oID){
+//   var at = new ActivityTemplate({
+//     // 'owner.ownerType': oType,
+//      'owner.ownerID': oID
+//   });
+//   at.save(function (err) {
+//         if (err) {
+//             return err;
+//         }
+//         else {
+//             console.log("New activity template created.");
+//         }
+//     });
+//   return(at);
+// };
+activityTemplateSchema.methods.addQuestion = function(cont){
    var question = new Question();
-   question.questionType = qType;
+  // question.questionType = qType;
    question.content = cont;
    question.save();
    this.questions.push(question);
